821a517d116a8884803d3568333e078e08d16bb2
iliyastb <skiliyas300@gmail.com>
2024-03-22 21:02:08 +0530

    Update

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
+# vpc
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   tags = merge(
     var.tags,
     {Name = "${var.env}-vpc"}
+  )
+}
+
+# peering
+resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.account.account_id
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept = true
+  tags = merge(
+    var.tags,
+    {Name = "${var.env}-peer"}
   )
 }
 
@@ -55,6 +68,11 @@ resource "aws_route_table" "public-rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
+  route {
+    cidr_block = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
   for_each = var.public_subnets
   tags = merge(
     var.tags,
@@ -85,6 +103,16 @@ resource "aws_subnet" "private_subnets" {
 resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw["public-${split("-", each.value["name"][1])}"].id
+  }
+
+  route {
+    cidr_block = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
   for_each = var.private_subnets
   tags = merge(
     var.tags,
@@ -97,5 +125,3 @@ resource "aws_route_table_association" "private-ass" {
   subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
   route_table_id = lookup(lookup(aws_route_table.private-rt, each.value["name"], null), "id", null)
 }
-
-
