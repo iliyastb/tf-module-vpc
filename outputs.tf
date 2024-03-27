8ae90d8d5e5185e97d132587fd7a12deac3023b7
iliyastb <skiliyas300@gmail.com>
2024-03-27 17:29:03 +0530

    Update

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "nat" {
+  value = aws_nat_gateway.natgw
+}
+
+output "public_subnets" {
+  value = aws_subnet.public_subnets
+}
+
+output "private_subnets" {
+  value = aws_subnet.private_subnets
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
